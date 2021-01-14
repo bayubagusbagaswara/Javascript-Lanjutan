939291518482282445e8d537f4535ead8513befa
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-14 13:14:12 +0700

    Mengganti function biasa menjadi arrow function

--- 3.2 this pada Arrow Function/script.js
+++ 3.2 this pada Arrow Function/script.js
@@ -4,16 +4,15 @@ const Mahasiswa = function () {
   this.umur = 24;
   this.sayHello = function () {
     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
-    // karena Function Expression memiliki konsep This, maka this akan mengacu ke Object Mahasiswa, dan memiliki this.nama dan this.umur
-    // Function Expression tidak akan kena hoisting, karena disimpan dalam variabel
   };
 
-  // Kasus lagi, jika menambahkan function yang otomatis dijalankan. Setiap interval akan menjalankan isi functionnya.
-  // Menggunakan Function Declaration, dan akan kena hoisting. Sehingga pada saat this dipanggil, dia akan mengecek ke global
-  setInterval(function () {
+  // Karena di global tidak ada this.umur maka hasilnya NaN
+  // Untuk mengatasi hal tersebut gunakan arrow function
+  // Karena arrow function tidak punya konsep this, maka this akan mencari ke lexical scopenya. Jadi, this.umur akan mengacu ke 24
+
+  setInterval(() => {
     console.log(this.umur++);
-    // Hasilnya NaN
+    // Hasilnya 24++
   }, 500);
-  //
 };
 const objectBayu = new Mahasiswa();
