7c8413894ce1c58cc8586ed720b9ca39df43f388
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-14 20:48:56 +0700

    buat object dengan function declaration

--- 1.1 Object/script.js
+++ 1.1 Object/script.js
@@ -1,26 +1,29 @@
-/* ===== Object Literal ===== */
-let mahasiswa1 = {
-  // properti
-  nama: "Bayu",
-  energi: 10,
-  // method
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
+/* ===== Function Declaration ===== */
+function Mahasiswa(nama, energi) {
+  // deklarasikan dulu kalo ingin buat object dengan object kosongan
+  let mahasiswa = {};
+  // isi properti
+  mahasiswa.nama = nama;
+  mahasiswa.energi = energi;
+  // isi method
+  mahasiswa.makan = function (porsi) {
+    this.energi += porsi;
     console.log(`Halo ${this.nama}, selamat makan!`);
-  },
-};
-let mahasiswa2 = {
-  // properti
-  nama: "Fauziah",
-  energi: 30,
-  // method
-  makan: function (porsi) {
-    this.energi = this.energi + porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  },
-};
+  };
+  mahasiswa.main = function (jam) {
+    this.energi -= jam;
+    console.log(`Halo ${this.nama}, selamat main!`);
+  };
+  // return
+  return mahasiswa;
+}
+// instansiasi object
+let object1 = Mahasiswa("Bayu", 10);
+let object2 = Mahasiswa("Fauziah", 50);
 
 /* ===== Noted 
-- Objectnya lansung diisi oleh properti dan method di dalamnya
-- untuk instansiasi object berikutnya, mau gak mau harus dicopy selanjutnya
+- cukup bikin template/blueprint satu aja
+- kalau ingin bikin object baru, tinggal instansiasi namanya aja
+- biasanya ketika instansiasi object akan sekaligus mengirim argument, yang kemudian dijadikan sebagai parameter function nya
+- harus ada return, karena untuk memasukkan/mengembalikan data-data object (nama,energi) ke object utamanya
 ===== */
