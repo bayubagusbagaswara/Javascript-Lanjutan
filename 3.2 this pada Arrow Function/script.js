ca5be070462a0a365057ad61805e5d35ab6ceb74
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-14 13:01:55 +0700

    test this arrow function menggunakan Object Literal

--- 3.2 this pada Arrow Function/script.js
+++ 3.2 this pada Arrow Function/script.js
@@ -1,26 +1,12 @@
-// jika buat Constructor Function
-// sebenarnya sama dengan cara membuat arrow function
-const Mahasiswa = function () {
-  // bikin properti
-  this.nama = "Bayu";
-  this.umur = 33;
-  //   this ini akan mengacu langsung pada Object Mahasiswa
-
-  this.sayHello = function () {
+// test konsep this arrow function menggunakan Object Literal
+const mhs1 = {
+  nama: "Bayu",
+  umur: 24,
+  sayHello: () => {
     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
-  };
+    // hasil console log adalah undefined
+    // Karena this tidak mencari ke lexical scopenya.
+    // Dan Arrow Function tidak memiliki konsep this, maka this.nama dan this.umur akan mencari keatas.
+    // dia akan mencari terus sampai ke window. Karena di window tidak ditemukan this.umur dan this.nama, maka hasilnya undefined.
+  },
 };
-// instansiasi Objectnya
-const objectBayu = new Mahasiswa();
-
-// ubah Constructor menjadi Arrow Function
-
-// arrow function tidak memiliki konsep this
-
-// test konsep this arrow function dengan Object Literal
-
-// function expression yang disimpan dulu kedalam variabel maka function tersebut tidak akan menerima hoisting. Dan function expression memiliki konsep this, sehingga isi dari function berupa this akan mencari langsung keatasnya.
-
-// sebaliknya jika function tersebut adalah function deklaration (tidak disimpan dalam variabel) maka akan kena hoisting
-
-// Untuk mengakali function declaration tersebut maka diubah menjadi arrow function. Karena arrow function tidak memiliki konsep this, maka this akan mencari ke lexical scopenya(bukan diglobal)
