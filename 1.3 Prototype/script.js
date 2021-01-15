56ba69e7b2da2bac4320f83b6c978d083fdc62cb
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 10:09:26 +0700

    Prototype untuk tipe data di Javascript

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,33 +1,17 @@
-// ubah konsep Prototype Inheritance menjadi Class pada konsep Object Oriented Programming
+// Prototype untuk tipe data di Javascript
 
-class Mahasiswa {
-  // CONSTRUCTOR : method wajib yang akan dijalankan ketika instansiasi object pertama kali, berisi properti yang wajib/melekat pada object
-  constructor(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-  }
+let arrayAngka = [];
+// dibelakang layar sebenarnya
+let arrayAngka = new Array();
 
-  // METHOD TAMBAHAN : method nya langsung ditulis dibawah
-  makan(porsi) {
-    this.energi += porsi;
-    return `Halo ${this.nama}, selamat makan!`;
-  }
-  main(jam) {
-    this.energi -= jam;
-    return `Halo ${this.nama}, selamat bermain!`;
-  }
-  tidur(jam) {
-    this.energi += jam * 2;
-    return `Halo ${this.nama}, selamat tidur!`;
-  }
+// Jadi Array itu sebuah Object yang secara otomatis membawa Prototype
+// didalam Javascriptnya prosesnya seperti dibawah
+function Array() {
+  // constructor
+  let this = Object.create(Array.prototype);
 }
 
-// instansiasi object baru
-let object1 = new Mahasiswa("Bayu", 10);
-let object2 = new Mahasiswa("Fauziah", 50);
-
-/* ===== Noted 
-- Prototype ini mirip dengan Kelas untuk Object Oriented Programming
-- Tapi, kenapa Javascript tidak menganut konsep Kelas? karena javascript hanya dibuat selama 1 minggu.
-- sebenarnya versi kelas ini, dibelakang layar menjalankan Prototype
+/* ===== Contoh method array yang merupakan prototype dari Object Array 
+- reverse
+- sort
 ===== */
