56f8b75c3c625a8ab8a7a3a1a3e924529dada2d6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 09:37:12 +0700

    membuat prototype dari object

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,12 +1,24 @@
 /* ===== dari code sebelumnya ===== */
 // PROBLEM sebelumnya : dengan membuat Object.create() seolah kita mengelola 2 object yang berbeda yaitu Object Mahasiswa dan Object Method Mahasiswa. Dan sebenarnya kita hanya menginginkan Object Mahasiswa nya saja.
 
-// Di depan layar hanya ditulis dibawah ini
+// Memanfaatkan method untuk mengelola method
 function Mahasiswa(nama, energi) {
   this.nama = nama;
   this.energi = energi;
 }
+Mahasiswa.prototype.makan = function (porsi) {
+  this.energi += porsi;
+  return `Halo ${this.nama}, selamat makan!`;
+};
+Mahasiswa.prototype.main = function (jam) {
+  this.energi -= jam;
+  return `Halo ${this.nama}, selamat bermain!`;
+};
+Mahasiswa.prototype.tidur = function (jam) {
+  this.energi += jam * 2;
+  return `Halo ${this.nama}, selamat tidur!`;
+};
 let object = new Mahasiswa("Bayu", 10);
 
-// PROBLEM sekarang: bagaimana mengurusi method nya?
-// Mengatasinya cukup memanfaatkan Prototype nya saja
+// method makan sudah otomatis dimasukkan kedalam Prototype, dan langsung menempel dengan Object Mahasiswa.
+// Sehingga jika kita tulis object.makan(3), maka methodnya langsung terpanggil
