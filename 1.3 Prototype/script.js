60534b0a872e3f29a456aebbc7b08e2555d07760
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 09:11:48 +0700

    function declaration diubah menjadi construction function

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,13 +1,9 @@
 /* ===== dari code sebelumnya ===== */
 // PROBLEM sebelumnya : dengan membuat Object.create() seolah kita mengelola 2 object yang berbeda yaitu Object Mahasiswa dan Object Method Mahasiswa. Dan sebenarnya kita hanya menginginkan Object Mahasiswa nya saja.
 
-// Function declaration biasa
+// Function declaration biasa ubah menjadi Construction Function
 function Mahasiswa(nama, energi) {
-  // inisialisasi variabel
-  let mahasiswa = {};
-
   mahasiswa.nama = nama;
   mahasiswa.energi = energi;
-
-  return mahasiswa;
 }
+// kalau mau diubah jadi construction function, tinggal hapus inisialiasi variabel untuk object nya, dan juga hapus returnya. Tapi, karena inisialiasi variabel dihilangkan, maka tidak punya akses lagi ke variabel mahasiswa/properti mahasiswa.
