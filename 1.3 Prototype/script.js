048405b78fe0ac13194f118ac9e713ef2d25930e
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 09:13:44 +0700

    ganti properti mahasiswa menjadi this

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,9 +1,8 @@
 /* ===== dari code sebelumnya ===== */
 // PROBLEM sebelumnya : dengan membuat Object.create() seolah kita mengelola 2 object yang berbeda yaitu Object Mahasiswa dan Object Method Mahasiswa. Dan sebenarnya kita hanya menginginkan Object Mahasiswa nya saja.
 
-// Function declaration biasa ubah menjadi Construction Function
+// Mengatasi PROBLEM: ganti properti/mahasiswa menjadi this
 function Mahasiswa(nama, energi) {
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
+  this.nama = nama;
+  this.energi = energi;
 }
-// kalau mau diubah jadi construction function, tinggal hapus inisialiasi variabel untuk object nya, dan juga hapus returnya. Tapi, karena inisialiasi variabel dihilangkan, maka tidak punya akses lagi ke variabel mahasiswa/properti mahasiswa.
