1633db650511c0bd839550eabca354f2baf1496f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 09:22:56 +0700

    construction function default memiliki prototype

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,13 +1,18 @@
 /* ===== dari code sebelumnya ===== */
 // PROBLEM sebelumnya : dengan membuat Object.create() seolah kita mengelola 2 object yang berbeda yaitu Object Mahasiswa dan Object Method Mahasiswa. Dan sebenarnya kita hanya menginginkan Object Mahasiswa nya saja.
 
-// Proses dibelakang layar Javascript membuat this dan return
+// Proses dibelakang layar Javascript membuat Object.create
 function Mahasiswa(nama, energi) {
-  let this = {};
+  let this = Object.create(Mahasiswa.prototype);
 
   this.nama = nama;
   this.energi = energi;
 
   return this;
 }
-// sebenarnya dibelakang layar, Javascript mengeksekusi dengan membuat inisialisasi variabel this dan return this.
+// dan sebenarnya yg terjadi adalah Javascriptnya membuat Object.create()
+// Tapi, didalam Object.create() sebenarnya Javascript memanggil Object Mahasiswa.prototype
+// Jadi, sebenarnya ada sebuah properti yang default dijadikan Parent oleh Javascriptnya
+// Itu terjadi di belakang layar
+// Sebenarnya Construction Function itu mempunyai Parent yang namanya adalah Prototype
+// Jadi, tidak perlu bikin object baru / object lain yang kita jadikan sebagai parent Object Utama
