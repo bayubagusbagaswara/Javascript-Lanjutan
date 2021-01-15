d8bc7f2f7a79e8dc251a8d494fc13666fa50e586
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 09:16:53 +0700

    proses javascript membuat this

--- 1.3 Prototype/script.js
+++ 1.3 Prototype/script.js
@@ -1,8 +1,13 @@
 /* ===== dari code sebelumnya ===== */
 // PROBLEM sebelumnya : dengan membuat Object.create() seolah kita mengelola 2 object yang berbeda yaitu Object Mahasiswa dan Object Method Mahasiswa. Dan sebenarnya kita hanya menginginkan Object Mahasiswa nya saja.
 
-// Mengatasi PROBLEM: ganti properti/mahasiswa menjadi this
+// Proses dibelakang layar Javascript membuat this dan return
 function Mahasiswa(nama, energi) {
+  let this = {};
+
   this.nama = nama;
   this.energi = energi;
+
+  return this;
 }
+// sebenarnya dibelakang layar, Javascript mengeksekusi dengan membuat inisialisasi variabel this dan return this.
