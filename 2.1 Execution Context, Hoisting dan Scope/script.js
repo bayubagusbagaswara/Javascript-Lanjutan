ac348cc802ef8228a07620abb1a7a84c474211ff
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-15 14:25:49 +0700

    uji fase creation dan execution

--- 2.1 Execution Context, Hoisting dan Scope/script.js
+++ 2.1 Execution Context, Hoisting dan Scope/script.js
@@ -1,6 +1,14 @@
 console.log(nama);
+var nama = "Bayu";
 
 /* ===== Noted
-- hasilnya akan ERROR
-- KENAPA? karena javascript tidak mengenali variabel nama
+- hasilnya UNDEFINED tapi TIDAK ERROR
+- KENAPA? karena variabel nama sudah di HOISTING terlebih dulu (FASE CREATION) dan diisi UNDEFINED , tetapi keburu di jalankan console.log (FASE EXECUTION) sebelum variabel nama sempat diisi nilainya yakni String Bayu.
+===== */
+
+/* ===== urutan prosesnya 
+1. Javacript akan mengecek var dan function. Dihoisting diisi dengan undefined
+2. Jalankan console.log -> hasilnya undefined
+3. Baru variabel nama diisi dengan nilai Bayu.
+Jadi, kedahuluan console.log sebelum variabel nama ada isinya
 ===== */
