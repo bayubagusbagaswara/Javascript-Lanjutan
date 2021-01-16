69130ef94c15343369526b4601ae4e7464a643a5
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-16 20:23:16 +0700

    object Arguments Javascript

--- 2.1 Execution Context, Hoisting dan Scope/script.js
+++ 2.1 Execution Context, Hoisting dan Scope/script.js
@@ -1,21 +1,20 @@
-/* ===== Kasus SCOPE ===== */
+/* ===== Arguments ===== */
 let nama = "Bayu";
 let username = "@bayu_bagaswara";
 
 function cetakURL() {
+  console.log(arguments); // cek object arguments
   let twitterURL = "http://twitter.com/";
   return twitterURL + username;
 }
 
-console.log(cetakURL("dimas_widyatama"));
+console.log(cetakURL("dimas_widyatama", "bambang"));
 
 // Hasilnya adalah http://twitter.com/@bayu_bagaswara
 
 /* ===== Noted  
-- Jika parameter function dihapus dan tetap mengirimkan argument, maka argument tersebut tidak ada yang nangkep.
-- Terus kemana isi argument tersebut? Akan dimasukkan kedalam Object di Javascript yang bernama Arguments.
+- Isi argument yang dikirimkan bersama dengan pemanggilan function sebenarnya tidak hilang, tapi disimpan dalam Object Javascript bernama Arguments.
 
-- Untuk variabel di dalam function, pertama kali dia akan mengecek pada parameter function yakni LOCAL. Jika tidak ada, maka variabel tersebut akan mengecek ke luar (GLOBAL), sampai ketemu varibel yang cocok. 
+- Apapun yang tidak ditangkep PARAMETER FUNCTION, maka secara langsung akan disimpan dalam Arguments
 
-- dan jika di GLOBAL tetep tidak ada, maka akan ERROR karena tidak terdefinisi/tidak ada referensi variabel tersebut
 ===== */
