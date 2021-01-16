abf684116d2b113550f99140897c93f312194d92
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-16 20:19:31 +0700

    reference scope global

--- 2.1 Execution Context, Hoisting dan Scope/script.js
+++ 2.1 Execution Context, Hoisting dan Scope/script.js
@@ -2,19 +2,20 @@
 let nama = "Bayu";
 let username = "@bayu_bagaswara";
 
-function cetakURL(username) {
+function cetakURL() {
   let twitterURL = "http://twitter.com/";
   return twitterURL + username;
 }
 
 console.log(cetakURL("dimas_widyatama"));
 
-// Hasilnya adalah http://twitter.com/dimas_widyatama
+// Hasilnya adalah http://twitter.com/@bayu_bagaswara
 
 /* ===== Noted  
-- KENAPA Hasilnya beda dari kasus awal? karena SCOPE
-- begitu memanggil function sekaligus mengirim argument
-- maka argument tersebut akan menjadi parameter bagi function itu sendiri
-- Sehingga nilai parameter itulah yang menjadi rujukan untuk isi program function tersebut
+- Jika parameter function dihapus dan tetap mengirimkan argument, maka argument tersebut tidak ada yang nangkep.
+- Terus kemana isi argument tersebut? Akan dimasukkan kedalam Object di Javascript yang bernama Arguments.
 
+- Untuk variabel di dalam function, pertama kali dia akan mengecek pada parameter function yakni LOCAL. Jika tidak ada, maka variabel tersebut akan mengecek ke luar (GLOBAL), sampai ketemu varibel yang cocok. 
+
+- dan jika di GLOBAL tetep tidak ada, maka akan ERROR karena tidak terdefinisi/tidak ada referensi variabel tersebut
 ===== */
