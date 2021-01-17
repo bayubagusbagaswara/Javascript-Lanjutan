c08b51213757a0968a52274eadb6bee00245af13
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-17 21:42:08 +0700

    simpan variabel counter dalam function, tapi muncul problem lagi

--- 2.2 Closure/script.js
+++ 2.2 Closure/script.js
@@ -2,11 +2,15 @@
 
 let counter = 0;
 let add = function () {
+  let counter = 0;
   return ++counter;
 };
 
-// isi ulang counter / mengubah counter awal
-counter = 10;
+// isi ulang counter / mengubah counter awal / restart
+counter = 100;
+
+console.log(add());
+console.log(add());
 console.log(add());
 
 /* ===== Noted
@@ -14,4 +18,6 @@ console.log(add());
 - jika program kalian panjang, tiba-tiba ada baris yang mengubah counter
 - dan karena HOISTING, counter di dalam function jadi terpengaruh
 
+- SOLUSI : agar counter tidak terganggu, sebenernya bisa simpan variabel counter di dalam function nya
+- TAPI PROBLEMNYA JADI : hasilnya 1 semua. Karena, setiap manggil add(), counter akan diisi 0 kemudian tambah 1, dan terus begitu selanjutnya
 ===== */
