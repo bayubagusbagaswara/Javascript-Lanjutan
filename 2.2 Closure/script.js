0d4e42d311678123a448a4a21930375928c95f94
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-17 21:38:11 +0700

    isi ulang counter

--- 2.2 Closure/script.js
+++ 2.2 Closure/script.js
@@ -1,28 +1,17 @@
-/* ===== Function Factories ===== */
+/* ===== Seolah-olah mempunyai Private Method ===== */
 
-function ucapkanSalam(waktu) {
-  return function (nama) {
-    console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan!`);
-  };
-}
+let counter = 0;
+let add = function () {
+  return ++counter;
+};
 
-// simpan kedalam variabel, untuk menjalankan dulu function OUTER nya (paling luar). Disini function sudah jalan setengahnya
-let selamatPagi = ucapkanSalam("Pagi");
-let selamatSiang = ucapkanSalam("Siang");
-let selamatMalam = ucapkanSalam("Malam");
+// isi ulang counter / mengubah counter awal
+counter = 10;
+console.log(add());
 
-// menjalankan setengah lagi function ucapkanSalam dan mengirimkan argument untuk ditangkep parameter nama
-selamatPagi("Bayu");
-selamatSiang("Bagus");
-selamatMalam("Bagaswara");
+/* ===== Noted
+- menghitung sudah berapa kali tombol itu di klik
+- jika program kalian panjang, tiba-tiba ada baris yang mengubah counter
+- dan karena HOISTING, counter di dalam function jadi terpengaruh
 
-/* ===== Noted :
-- kalau kita jalankan function ucapkanSalam dengan mengirimkan argument, maka akan lansung ditangkap oleh parameter WAKTU, ini disimpan kedalam CLOSURE
-- Tetapi, di dalam function ucapkanSalam hanya berisi return function ()
-- Dari situ maka ucapkanSalam() hanya dieksekusi sebagian.
-- Oleh karena itu, simpan function ucapkanSalam() kedalam sebuah variabel, misal let selamatPagi
-- Dan untuk menjalankan sebagiannya lagi, maka jalankan variabel function selamatPagi() tersebut 
-- Jalankan variabel function selamatPagi('Bayu') dengan diisi argument untuk dikirimkan menjadi parameter nama.
-
-- FACTORIES FUNCTION -> bikin function sesuai dengan function yang lain
 ===== */
