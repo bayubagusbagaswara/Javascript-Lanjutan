6e69231a31a66b9119805b59238686b3a6d047e0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-17 21:30:33 +0700

    factories function

--- 2.2 Closure/script.js
+++ 2.2 Closure/script.js
@@ -1,16 +1,28 @@
-/* ===== Function Factory ===== */
-function init() {
+/* ===== Function Factories ===== */
+
+function ucapkanSalam(waktu) {
   return function (nama) {
-    console.log(nama);
+    console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan!`);
   };
 }
-let panggilNama = init();
-panggilNama("Bayu");
-panggilNama("Bagus");
-panggilNama("Bagaswara");
 
-// Hasilnya adalah Bayu, Bagus, Bagaswara
+// simpan kedalam variabel, untuk menjalankan dulu function OUTER nya (paling luar). Disini function sudah jalan setengahnya
+let selamatPagi = ucapkanSalam("Pagi");
+let selamatSiang = ucapkanSalam("Siang");
+let selamatMalam = ucapkanSalam("Malam");
 
-/* ===== Noted 
-- apabila hanya mengembalikan function, bisa diringkas lagi tanpa nama function, alias anonymous function. Atau function tanpa nama hanya menerima parameter.
+// menjalankan setengah lagi function ucapkanSalam dan mengirimkan argument untuk ditangkep parameter nama
+selamatPagi("Bayu");
+selamatSiang("Bagus");
+selamatMalam("Bagaswara");
+
+/* ===== Noted :
+- kalau kita jalankan function ucapkanSalam dengan mengirimkan argument, maka akan lansung ditangkap oleh parameter WAKTU, ini disimpan kedalam CLOSURE
+- Tetapi, di dalam function ucapkanSalam hanya berisi return function ()
+- Dari situ maka ucapkanSalam() hanya dieksekusi sebagian.
+- Oleh karena itu, simpan function ucapkanSalam() kedalam sebuah variabel, misal let selamatPagi
+- Dan untuk menjalankan sebagiannya lagi, maka jalankan variabel function selamatPagi() tersebut 
+- Jalankan variabel function selamatPagi('Bayu') dengan diisi argument untuk dikirimkan menjadi parameter nama.
+
+- FACTORIES FUNCTION -> bikin function sesuai dengan function yang lain
 ===== */
