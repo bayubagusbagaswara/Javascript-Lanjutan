03f1c70544f1a2c4227d21ea93b59b753376a9e7
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 11:11:54 +0700

    contoh Higher Order Function

--- Higher Order Function/script.js
+++ Higher Order Function/script.js
@@ -1,20 +1,39 @@
-/* ===== Contoh 1 ===== */
-function kerjakanTugas(matakuliah, selesai) {
-  console.log(`Mulai mengerjakan tugas ${matakuliah} ...`);
-  // panggil function selesai()
-  selesai();
-}
+/* ========== Contoh 2 ========== */
 
-// menggunakan function selesai() sebagai argument
-function selesai() {
-  alert("Selesai mengerjakan tugas!");
-}
-// panggil function kerjakanTugas sekaligus kirim parameter 1 berupa STRING dan parameter 2 berupa FUNCTION
-kerjakanTugas("Pemrograman Web", selesai);
+setTimeout(function () {
+  console.log("Hello World!");
+}, 1000);
 
 /* ===== Noted 
-- Parameter matakuliah disimpan berupa STRING, lalu Parameter selesai yang merupakan function.
-- kita bisa menyimpan function lagi sebagai argument nya
-- Jadi, function kerjakanTugas bisa disebut dengan Higher Order Function atau bisa dikatakan function yang memiliki Callback
-- dan jika punya function sebagai argument disebut dengan Callback
- ===== */
+- method setTimeout ini mempunyai 2 parameter :
+- parameter 1 adalah function()
+- parameter 2 adalah set waktunya (milisecond waktu tunggu)
+- Karena parameter 1 adalah function / Callback, maka setTimeout disebut dengan Higher Order Function. 
+===== */
+
+/* ========== Contoh 3 ========== */
+const tombol = document.querySelector(".submit");
+tombol.addEventListener("click", function () {
+  console.log("tombol ditekan!");
+});
+
+/* ===== Noted 
+- begitu tombol diklik lalu melakukan aksi tertentu.
+- pakai DOM bisa ambil elementnya dulu.
+- lalu kasih addEventListener, lalu eventnya apa dan callbacknya apa.
+===== */
+
+/* ========== Contoh 4 ========== */
+function ucapkanSalam(waktu) {
+  // return value berupa function
+  return function (nama) {
+    console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
+  };
+}
+let selamatMalam = ucapkanSalam("Malam");
+console.dir(selamatMalam("Bayu"));
+
+// kita juga bisa punya function yang return value nya adalah function lagi
+/* ===== Noted
+- function ucapkanSalam bisa disebut Higher Order Function, karena didalamnya memiliki return value yang juga sebuah functions
+===== */
