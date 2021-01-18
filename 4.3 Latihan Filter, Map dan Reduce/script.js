8a55716239697ff7d3d41bd32918ede45c69f65a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 20:07:12 +0700

    hitung total durasinya Jam Menit Detik

--- 4.3 Latihan Filter, Map dan Reduce/script.js
+++ 4.3 Latihan Filter, Map dan Reduce/script.js
@@ -1,16 +1,27 @@
 const videos = Array.from(document.querySelectorAll("[data-duration]"));
-const jsLanjut = videos
+let jsLanjut = videos
   .filter((video) => video.textContent.includes("Javascript Lanjutan"))
   .map((item) => item.dataset.duration)
   .map((waktu) => {
     const parts = waktu.split(".").map((part) => parseFloat(part));
     return parts[0] * 60 + parts[1];
   })
-  // total detik
   .reduce((total, detik) => total + detik);
 
-console.log(jsLanjut);
+//   ubah format waktu
+const jam = Math.floor(jsLanjut / 3600);
+// jsLanjut = total detik awal - 2 jam * 3600 -> hasilnya detik
+jsLanjut = jsLanjut - jam * 3600;
+// jsLanjut dibagi 60
+const menit = Math.floor(jsLanjut / 60);
+// hitung detik
+const detik = jsLanjut - menit * 60;
+
+console.log(detik);
 
 /* =============== Noted
-- reduce untuk menjumlahkan semua waktu dalam video
+- ubah formatnya menjadi Jam Menit Detik
+- pertama bagi waktu menjadi jam dulu dengan bagi 3600 dan dibulatkan Math.floor(pembulatan ke bawah)
+- sisanya hitung untuk menit. jsLanjut - 2 * 3600. Hasilnya detik, kemudia jadikan menit dengan cara bagi 60
+- sisanya lagi adalah detik
 =============== */
