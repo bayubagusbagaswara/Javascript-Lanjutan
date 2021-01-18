b6c92dc281641effeb0f68a81a6c01df81a6408f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 20:15:40 +0700

    finally code

--- 4.3 Latihan Filter, Map dan Reduce/script.js
+++ 4.3 Latihan Filter, Map dan Reduce/script.js
@@ -13,27 +13,12 @@ jsLanjut = jsLanjut - jam * 3600;
 const menit = Math.floor(jsLanjut / 60);
 const detik = jsLanjut - menit * 60;
 
-// simpan di DOM
-// masukkan jam menit detik
 const pDurasi = document.querySelector(".total-durasi");
 pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik`;
 
-// hitung jumlah video
 const jmlVideo = videos.filter((video) =>
   video.textContent.includes("Javascript Lanjutan")
 ).length;
 const pJmlVideo = document.querySelector(".jumlah-video");
 pJmlVideo.textContent = `${jmlVideo} Video`;
 console.log(detik);
-
-/* =============== Noted
-- simpan di DOM
-- cari elemen class nya total-durasi
-- isi textContentnya dengan Jam Menit Detik
-
-- hitung jumlah Video
-- ambil data array yg sudah difilter tadi karena filter Javascript Lanjutan
-- kemudian hitung dengan tambahkan function length
-- cari elemen class jumlah-video
-- isi textContentnya dengan jumlah Video
-=============== */
