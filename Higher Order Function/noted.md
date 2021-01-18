f40a138447cec1ea0426511e71ad93220a2f3cf0
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 11:42:23 +0700

    abstraksi Higher Order Function

--- Higher Order Function/noted.md
+++ Higher Order Function/noted.md
@@ -8,3 +8,10 @@ Karena di dalam Javascript itu sebuah function itu disebut First Class Function.
 
 "Javascript memperlakukan function sebagai object."
 Object itu merupakan value sama seperti integer/string. Maka function juga bisa kita simpan sebagai argument maupun return value dari sebuah function yang lain.
+
+# Kenapa pake Higher Order Function ?
+
+1. Abstraksi, untuk membuat agar code yang kita buat itu bisa lebih sederhana/simple. Menggunakan function sama dengan menyembunyikan kerumitan.
+   "Semakin besar sebuah program, semakin tinggi kompleksitasnya, semakin membingungkan programmernya." - eloquentjavascript.net
+
+   "Ada dua cara untuk merancang sebuah software: Cara pertama adalah untuk membuat programnya se-sederhana mungkin sehingga jelas-jelas tidak ada kekurangannya, dan cara lainnya adalah untuk membuat programnya se-kompleks mungkin sehingga tidak ada kekurangan yang jelas." - C.A.R. Hoare, 1980 ACM Turing Award Lecture
--- Higher Order Function/script.js
+++ Higher Order Function/script.js
@@ -1,39 +1,20 @@
-/* ========== Contoh 2 ========== */
+/* =============== Contoh Abstraksi =============== */
 
-setTimeout(function () {
-  console.log("Hello World!");
-}, 1000);
-
-/* ===== Noted 
-- method setTimeout ini mempunyai 2 parameter :
-- parameter 1 adalah function()
-- parameter 2 adalah set waktunya (milisecond waktu tunggu)
-- Karena parameter 1 adalah function / Callback, maka setTimeout disebut dengan Higher Order Function. 
-===== */
-
-/* ========== Contoh 3 ========== */
-const tombol = document.querySelector(".submit");
-tombol.addEventListener("click", function () {
-  console.log("tombol ditekan!");
-});
-
-/* ===== Noted 
-- begitu tombol diklik lalu melakukan aksi tertentu.
-- pakai DOM bisa ambil elementnya dulu.
-- lalu kasih addEventListener, lalu eventnya apa dan callbacknya apa.
-===== */
-
-/* ========== Contoh 4 ========== */
-function ucapkanSalam(waktu) {
-  // return value berupa function
-  return function (nama) {
-    console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
-  };
+let total = 0,
+  count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+  console.log(total);
 }
-let selamatMalam = ucapkanSalam("Malam");
-console.dir(selamatMalam("Bayu"));
+// console.log(total);
 
-// kita juga bisa punya function yang return value nya adalah function lagi
-/* ===== Noted
-- function ucapkanSalam bisa disebut Higher Order Function, karena didalamnya memiliki return value yang juga sebuah functions
-===== */
+// Hasilnya 55
+
+/* =============== Noted
+- menghitung sebuah
+- selamat count <= 10 
+- maka isi total dengan dijumlahkan dengan count
+- kemudian isi count ditambahkan dengan 1 (atau hasil count += 1)
+
+=============== */
