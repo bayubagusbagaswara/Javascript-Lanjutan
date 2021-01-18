2bc7dd59e7224736d5894529a1ccc7d0897ec215
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 12:43:26 +0700

    function repeat

--- Higher Order Function/noted.md
+++ Higher Order Function/noted.md
@@ -15,3 +15,13 @@ Object itu merupakan value sama seperti integer/string. Maka function juga bisa 
    "Semakin besar sebuah program, semakin tinggi kompleksitasnya, semakin membingungkan programmernya." - eloquentjavascript.net
 
    "Ada dua cara untuk merancang sebuah software: Cara pertama adalah untuk membuat programnya se-sederhana mungkin sehingga jelas-jelas tidak ada kekurangannya, dan cara lainnya adalah untuk membuat programnya se-kompleks mungkin sehingga tidak ada kekurangan yang jelas." - C.A.R. Hoare, 1980 ACM Turing Award Lecture
+
+# Functional Programming
+
+- program kita akan jadi lebih efektif dan efisien
+
+# Contoh Higher Order Function, khususnya untuk pengelolaan Array
+
+1. Array.prototype.map()
+2. Array.prototype.filter()
+3. Array.prototype.reduce()
--- Higher Order Function/script.js
+++ Higher Order Function/script.js
@@ -1,9 +1,28 @@
 /* =============== Contoh Abstraksi =============== */
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+function repeatLog(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
 }
+repeatLog(10);
 
 /* =============== Noted
-- Bagaimana kalau ingin melooping sebanyak 100x, 100x ? harus merubah secara manual (tidak dinamis)
+- nulis for nya cuma sekali, ganti angka 10 dengan variabel n
+- variabel n menjadi argument dari function 
+- jika kita ingin mengulang sesuai dengan yang diinginkan, tinggal panggil function dan dikirimkan parameter berapa kali pengulangannya.
+=============== */
 
+/* =============== Jika ingin Action lain =============== */
+function repeat(n, action) {
+  for (let i = 0; i < n; i++) {
+    action(i); // jalankan aksi beberapa kali sesuai jumlah perulangannya
+  }
+}
+repeat(10, console.log); // aksi console.log
+repeat(3, alert); // aksi alert
+
+/* =============== Noted 
+- function menerima paramater berupa n (jumlah perulangan) dan parameter action (aksi apa yg ingin dilakukan)
+- untuk menjalankan tinggal panggil function repeat sekaligus mengirimkan argument n dan argument action nya
+- jadi parameter keduanya adalah function 
 =============== */
