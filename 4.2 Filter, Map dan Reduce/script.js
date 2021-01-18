b429e94dc1f33e07c62f30c3a6eecf1bf6b06e8c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 13:16:38 +0700

    tambahkan arrow function untuk filter function

--- 4.2 Filter, Map dan Reduce/script.js
+++ 4.2 Filter, Map dan Reduce/script.js
@@ -2,9 +2,9 @@
 
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-// menggunakan Filter
-const newAngka = angka.filter(function (a) {
-  return a >= 3;
+// menggunakan Filter dan diubah menjadi Arrow Function
+const newAngka = angka.filter((a) => {
+  a >= 3;
 });
 
 console.log(newAngka);
