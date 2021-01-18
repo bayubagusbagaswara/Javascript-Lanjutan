bf3dba7edc146f4373e2fe8148b2703576adf818
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 15:05:24 +0700

    modified code filter, perhatikan penulisan arrow function

--- 4.2 Filter, Map dan Reduce/script.js
+++ 4.2 Filter, Map dan Reduce/script.js
@@ -1,20 +1,11 @@
 /* =============== Filter =============== */
-
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-// menggunakan Filter dan diubah menjadi Arrow Function
-const newAngka = angka.filter((a) => {
-  a >= 3;
-});
+const newAngka = angka.filter((a) => a >= 3);
 
 console.log(newAngka);
-// Hasilnya 8,9,4,3,9
 
 /* =============== Noted 
-- langsung panggil Array mana yang akan di filter.
-- dan filter memiliki callback function nya
-- a merepresentasikan tiap-tiap elemen di dalam Array Angka
-- elemen ke 1 = -1
-- elemen ke 2 = 8
-- elemen ke 3 = 9 dst...
+- Penulisan arrow function diperhatikan
+- kalau hanya return HILANGKAN KATA RETURN DAN KURAWAL {}, sehingga hanya jadi SEBARIS
 =============== */
