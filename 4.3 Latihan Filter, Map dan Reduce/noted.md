38e0d9bee2ee30bee40fa45b67e0ce2be462ecc1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 19:00:32 +0700

    dapatkan duration menggunakan dataset

---
+++ 4.3 Latihan Filter, Map dan Reduce/noted.md
@@ -0,0 +1,9 @@
+# Algoritma
+
+1. Ambil semua element video
+2. Pilih hanya yang bertulis Javascript Lanjutan
+3. Ambil durasi masing-masing video (masih berbentuk string)
+4. Ubah string durasi menjadi integer, ubah menit menjadi detik
+5. Jumlahkan semua detik
+6. Ubah format waktunya jadi Jam Menit Detik
+7. Simpan di DOM
--- 4.3 Latihan Filter, Map dan Reduce/script.js
+++ 4.3 Latihan Filter, Map dan Reduce/script.js
@@ -1,21 +1,13 @@
-/* =============== Algoritma
-1. Ambil semua element video
-2. Pilih hanya yang bertulis Javascript Lanjutan
-3. Ambil durasi masing-masing video (masih berbentuk string)
-4. Ubah string durasi menjadi integer, ubah menit menjadi detik
-5. Jumlahkan semua detik
-6. Ubah format waktunya jadi Jam Menit Detik
-7. Simpan di DOM
-=============== */
-
 const videos = Array.from(document.querySelectorAll("[data-duration]"));
-const jsLanjut = videos.filter((video) =>
-  video.textContent.includes("Javascript Lanjutan")
-);
-console.log(jsLanjut); // hasilnya ada 8
+const jsLanjut = videos
+  .filter((video) => video.textContent.includes("Javascript Lanjutan"))
+  .map((item) => item.dataset.duration);
+
+console.log(jsLanjut);
+// Hasilnya ["11.18", "21.40", "12.10", "20.43", "26.38", "17.33", "10.39", "17.31"]
 
 /* =============== Noted
-- Ubah dulu NodeList menjadi Array
-- filter semua videos nya dan cari yang ada tulisan Javascript Lanjutan
-- video adalah tiap elemen yang ada didalam array videos
+- ambil durasi masing-masing video
+- method dataset untuk mengambil atribut data-.....
+- gunakan chaining method
 =============== */
