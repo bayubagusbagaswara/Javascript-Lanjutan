b22392dc13db38ff67074e302bca09277e42733d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 15:08:28 +0700

    menggunakan map function

--- 4.2 Filter, Map dan Reduce/script.js
+++ 4.2 Filter, Map dan Reduce/script.js
@@ -1,11 +1,15 @@
-/* =============== Filter =============== */
+/* =============== Map =============== */
 const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 
-const newAngka = angka.filter((a) => a >= 3);
+const newAngka = angka.map((a) => {
+  return a * 2;
+});
 
 console.log(newAngka);
+// Hasilnya [-2, 16, 18, 2, 8, -10, -8, 6, 4, 18]
+console.log(angka); // tidak mengubah isi array angka
 
 /* =============== Noted 
-- Penulisan arrow function diperhatikan
-- kalau hanya return HILANGKAN KATA RETURN DAN KURAWAL {}, sehingga hanya jadi SEBARIS
+- kita akan memetakan tiap-tiap elemen di dalam array nya menggunakan fungsi yang baru
+- kalikan semua elemen array dengan 2, angka * 2 
 =============== */
