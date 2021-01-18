341beabd3359c49686be7234c9c322b1db61ae22
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-18 19:53:13 +0700

    parseFloat array parts

--- 4.3 Latihan Filter, Map dan Reduce/script.js
+++ 4.3 Latihan Filter, Map dan Reduce/script.js
@@ -3,17 +3,20 @@ const jsLanjut = videos
   .filter((video) => video.textContent.includes("Javascript Lanjutan"))
   .map((item) => item.dataset.duration)
   .map((waktu) => {
-    // durasi 10.30, pecah jadi array ["10", "30"] -> split
-    const parts = waktu.split(".");
-    // kalikan parts ke 0 dengan 60 ditambah
-    return parts;
+    // Ubah dulu string menjadi
+    const parts = waktu.split(".").map((part) => parseFloat(part));
+    // kalikan parts ke 0 dengan 60 ditambah parts ke 1. Hitung menjadi satuan detik
+    return parts[0] * 60 + parts[1];
   });
 console.log(jsLanjut);
 
 /* =============== Noted
 - ubah durasinya dari STRING menjadi INTEGER
 - parts adalah array yang menampung waktu [10,30]
+parts[0] = 10 menit, parts[1] = 30 detik
+- semua element parts harus diubah menjadi detik.
 - part adalah tiap elemen dalam array parts
+- part yang isinya masih berupa STRING harus diubah dulu menjadi bentuk integer/float agar bisa dijumlahkan
 
 
 =============== */
