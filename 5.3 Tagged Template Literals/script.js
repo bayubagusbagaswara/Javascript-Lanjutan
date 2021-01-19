f80f58d92e225f0df4427f2e68836b73e3d5ce4d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-19 13:20:07 +0700

    expression jadi paramete function

--- 5.3 Tagged Template Literals/script.js
+++ 5.3 Tagged Template Literals/script.js
@@ -2,7 +2,7 @@ const nama = "Bayu Bagaswara";
 const umur = 24;
 
 // function
-function coba(strings) {
+function coba(strings, nama, umur) {
   return strings;
 }
 const str = coba`Halo, nama saya ${nama}, saya${umur} tahun`;
@@ -11,5 +11,6 @@ console.log(str);
 /* =============== Noted
 - cara ubah template literals biasa menjadi tagged template, dengan menulisakan namafunction didepan string nya
 - Jadi string template literals, seolah-olah akan masuk jadi parameter pada function 
-- Parameter strings akan memecah string dari template literals yang dipisahkan dengan expression nya
+- Parameter strings akan memecah string dari template literals yang dipisahkan dengan expression nya. Disimpan dalam array
+- Expression menjadi parameter function
 =============== */
