c796c48426465d0dee0c66f08f48ed915c4c6ebd
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-19 13:17:03 +0700

    function tagged template

--- 5.3 Tagged Template Literals/script.js
+++ 5.3 Tagged Template Literals/script.js
@@ -1,8 +1,15 @@
 const nama = "Bayu Bagaswara";
 const umur = 24;
 
-const str = `Halo, nama saya ${nama}, saya${umur} tahun`;
+// function
+function coba(strings) {
+  return strings;
+}
+const str = coba`Halo, nama saya ${nama}, saya${umur} tahun`;
+console.log(str);
 
 /* =============== Noted
-- template literals
+- cara ubah template literals biasa menjadi tagged template, dengan menulisakan namafunction didepan string nya
+- Jadi string template literals, seolah-olah akan masuk jadi parameter pada function 
+- Parameter strings akan memecah string dari template literals yang dipisahkan dengan expression nya
 =============== */
