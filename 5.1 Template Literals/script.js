0c5943c8651e7feb818ae55dc5027e496686e70d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-19 11:33:09 +0700

    contoh penggunaan template literals

--- 5.1 Template Literals/script.js
+++ 5.1 Template Literals/script.js
@@ -1,68 +1,26 @@
-/* =============== Contoh Multi-Line String =============== */
-`string text baris 1
-string text baris 2
-string text baris 3`;
-// untuk memisah string \n, kalau menggunakan kutip biasa
-// console.log("string 1\nstring 2");
-// kalau menggunakan back tick cukup di enter aja
-// console.log(`string 1
-// string 2`);
-
-/* =============== Embedded Expression =============== */
-// `string text ${expression} string text`;
-
-/* =============== Tagged Template =============== */
-// tag`string text ${expression} string text`;
-
-/* =============== Multi-line String (HTML Fragments) (1) =============== */
-const mhs = {
-  nama: "Bayu Bagaswara",
-  umur: 24,
-  nrp: "2216030063",
-  email: "bagaszwara@gmail.com",
-};
-//  tampilkan data mhs diatas kedalam html, sebelumnya menggunakan concat, satu per satu dimasukkan kedalam variabel el
-// let el = "";
-// el += '<div class="mhs">';
-// el += "<h2>" + mhs.nama + "</h2>";
-// el += '<span class="nrp">' + mhs.nrp + "</span>";
-// el += "</div>";
-// console.log(el);
-// Hasil : <div class="mhs"><h2>Bayu Bagaswara</h2><span class="nrp">2216030063</span></div>
-
-// bikin di Template Literal, cukup tulis Back Tick langsung isi semua string HTML nya
-let el = `<div class="mhs">
-<h2>${mhs.nama}</h2>
-<span class="nrp">${mhs.nrp}</span>
-</div>`;
-console.log(el);
-// Hasil :
-/* <div class="mhs">
-<h2>Bayu Bagaswara</h2>
-<span class="nrp">2216030063</span>
-</div> */
-
-/* =============== Embedded Expression =============== */
-// menyimpan expression kedalam template literal
+// Template Literals atau Template String
 const nama = "Bayu";
 const umur = 24;
-console.log(`Halo, nama saya ${nama}, dan saya ${umur} tahun`);
+console.log(`Halo, nama saya ${nama}, dan saya umur ${umur} tahun.`);
 
-// Expression Interpolation (1)
-let a = 10;
-let b = 15;
-console.log(
-  "Jika a = 10 dan b = 15, maka hasil penjumlahannya adalah : " +
-    (a + b) +
-    ", bukan " +
-    (2 * a + b)
-);
-// Expression Interpolation (2)
-// code diatas dimasukkan ke template literal hanya menuliskan Back Tick
-let a = 10;
-let b = 15;
-console.log(
-  `Jika a = 10 dan b = 15, maka hasil penjumlahannya adalah : ${a + b}, bukan ${
-    2 * a + b
-  }`
-);
+// Embedded Expression, langsung melakukan expression
+console.log(`${1 + 1}`);
+// function termasuk expression juga
+// console.log(`${alert('halo')}`);
+// ternary operator juga expression
+const x = 10;
+console.log(`${x % 2 == 0 ? "genap" : "ganjil"}`);
+
+// HTML Fragments, bisa simpan kedalam html
+const mhs = {
+  nama: "bayu",
+  umur: 24,
+  nrp: "2216030063",
+  email: "bayu@gmail.com",
+};
+
+const el = `<div class="mhs">
+      <h2>${mhs.nama}</h2>
+      <span class="nrp">${mhs.nrp}</span>
+  </div>`;
+console.log(el);
