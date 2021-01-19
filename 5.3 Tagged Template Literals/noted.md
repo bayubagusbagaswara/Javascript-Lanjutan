5fe5a7f3cd23838206536bf3b65a0804011a1d28
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-19 13:12:15 +0700

    pengantar tagged literals

---
+++ 5.3 Tagged Template Literals/noted.md
@@ -0,0 +1,3 @@
+# Tagged Template Literals
+
+    "Bentuk yang lebih kompleks dari Template Literals, memungkinkan kita untuk membaca template literals melalui sebuah function."
---
+++ 5.3 Tagged Template Literals/script.js
@@ -0,0 +1,8 @@
+const nama = "Bayu Bagaswara";
+const umur = 24;
+
+const str = `Halo, nama saya ${nama}, saya${umur} tahun`;
+
+/* =============== Noted
+- template literals
+=============== */
