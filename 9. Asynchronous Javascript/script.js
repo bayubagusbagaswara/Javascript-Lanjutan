96c5acf77faa7057eba041f2958e0a75f318544f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-01-27 21:42:25 +0700

    asynchronous javascript dan callback

--- 9. Asynchronous Javascript/script.js
+++ 9. Asynchronous Javascript/script.js
@@ -31,12 +31,16 @@ cetakLuasKotak(5);
 
 /**
  * ! Noted : Urutan eksekusi program Contoh 2
- * 1. Pertama langsung panggil function @cetakLuasKotak sekaligus kirim argument. Kenapa langsung panggil cetakLuasKotak()? Karena, program lainnya berupa function yang artinya di Hoisting dan dimasukkan ke dalam Heap.
- * 2. Jalankan function @cetakLuasKotak dengan masuk kedalam function
+ * 1. Pertama langsung panggil function @cetakLuasKotak sekaligus kirim argument. Kenapa langsung panggil cetakLuasKotak()? Karena, program lain berupa function yang artinya di Hoisting dan dimasukkan ke dalam Heap.
+ * 2. Langsung jalankan function @cetakLuasKotak dan kirimkan parameter. Cari function @cetakLuasKotak , lanjut masuk ke function nya. Cari apakah ada varibel/function, teryata ada variabel yang juga tugasnya memanggil function @hitungLuas , hoisting dulu variabel @luas
+ * 3. Cari function @hitungLuas , masuk kedalam function nya. Ternyata didalamnya nya terdapat @return yang juga memanggil function @kalikan
+ * 4. Cari function @kalikan , masuk kedalam function nya. Di dalam function terdapat @return nilai a * b
+ * 5. Eksekusi dan kembalikan @a * @b = 25. Selesai eksekusi dan hilang dari tumpukan Stack Eksekusi
+ * 6. Kembali ke return function @kalikan di dalam function @hitungLuas , Eksekusi return dan hilang dari tumpukan Stack
+ * 7. Kembali ke function @hitungLuas didalam function @cetakLuasKotak , simpan hasil 25 kedalam variabel @luas , kemudian Eksekusi variabel @luas tampilkan hasilnya 25. Selesai dan hilangkan @consolelog dari tumpukan Stack
+ * 8. Selesai eksekusi function @cetakLuasKotak
  */
 
-
- 
 /* ===== Noted
 1. function kalikan masuk kedalam -> Heap (hoisting)
 2. hitungLuas masuk kedalam -> Heap
@@ -90,6 +94,7 @@ setTimeout(() => {
   console.log("dua");
 }, 5000);
 console.log("tiga");
+
 /* ===== Noted 
 1. tampilkan "satu"
 2. "dua" dijadikan function callback yang parameter delaynya menunggu selama 5 detik
@@ -109,6 +114,7 @@ console.log("tiga");
 3. Event Loop
 4. Callback Queue
 ===== */
+
 /* ===== 
 1. sebenarnya perintah di setTimeout dipindahkan ke Web API
 2. setelah waktunya selesai, Web API tidak bisa kembali menjalankan fungsinya kedalam Stack, sehingga callbacknya harus disimpan dulu kedalam tempat yang namanya Callback Queue(antrian untuk callback)
